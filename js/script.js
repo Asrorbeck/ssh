5ea3540853c3e509a2e2358f2639f2cb469fbd6e
Asrorbeck <2010288.asrorbek.tursunpulatov@gmail.com>
2024-06-23 11:47:22 +0500

    1

--- js/script.js
+++ js/script.js
@@ -26,3 +26,55 @@ function initializeCountdown(countdownId, targetDate) {
 
 initializeCountdown("1", "July 10, 2024 19:00:00");
 initializeCountdown("2", "July 11, 2024 19:00:00");
+
+const musicControl = document.getElementById("navbar__btn");
+const audio = document.getElementById("background-music");
+const musicIcon = document.getElementById("music-icon");
+
+let isPlaying = true;
+
+var modal = document.getElementById("myModal");
+var yesBtn = document.getElementById("yesBtn");
+var noBtn = document.getElementById("noBtn");
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+yesBtn.addEventListener("click", function () {
+  audio.muted = false;
+  audio.play();
+  musicIcon.src = "./img/unmute.svg";
+  closeModal();
+
+  isPlaying = !isPlaying;
+});
+
+noBtn.addEventListener("click", function () {
+  audio.pause();
+  musicIcon.src = "./img/mute.svg";
+  closeModal();
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
+
+window.onload = function () {
+  modal.style.display = "block";
+};
+
+musicControl.addEventListener("click", function () {
+  if (isPlaying) {
+    audio.muted = false;
+    audio.play();
+    musicIcon.src = "./img/unmute.svg";
+  } else {
+    audio.pause();
+    musicIcon.src = "./img/mute.svg";
+  }
+
+  isPlaying = !isPlaying;
+});
